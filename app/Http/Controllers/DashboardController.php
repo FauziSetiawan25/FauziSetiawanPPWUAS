6a77e05d0c16abfd52ebe62fe1dc87eba42a73ba
FauziSetiawan25 <fauzisetiawan2003@mail.ugm.ac.id>
2024-12-03 13:59:24 +0700

    Logika login dan menampilkan data pada dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,11 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Transaksi;
+use App\Models\TransaksiDetail;
+
 class DashboardController extends Controller
 {
     public function index()
     {
-        $transaksi_count =
-        return view('dashboard', );
+        // Menghitung jumlah transaksi
+        $transaksi_count = Transaksi::count();
+
+        // Menghitung jumlah item terjual
+        $jumlah_item_terjual = TransaksiDetail::sum('jumlah');
+
+        // Menghitung total omzet
+        $omzet = TransaksiDetail::sum('subtotal');
+
+        return view('dashboard', compact('transaksi_count', 'jumlah_item_terjual', 'omzet'));
     }
 }
