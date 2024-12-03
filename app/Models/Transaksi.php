12344dd3d3e918647fdd7636066029febd09ce40
FauziSetiawan25 <fauzisetiawan2003@mail.ugm.ac.id>
2024-12-03 13:40:04 +0700

    softdelete model transaksi

--- app/Models/Transaksi.php
+++ app/Models/Transaksi.php
@@ -4,20 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;  // Tambahkan ini
 
 class Transaksi extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;  // Menggunakan SoftDeletes
 
     protected $table = 'transaksi'; 
 
-    protected $fillable = [ 
+    protected $fillable = [
         'tanggal_pembelian',
         'total_harga',
         'bayar',
+        'kembalian', // pastikan kolom ini ada jika digunakan
     ];
 
-    
+    protected $dates = ['deleted_at'];  // Menentukan kolom yang digunakan untuk soft delete
+
     public function transaksidetail()
     {
         return $this->hasMany(TransaksiDetail::class, 'id_transaksi', 'id');
